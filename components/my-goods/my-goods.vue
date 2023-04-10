52d5494e30febe303ca9a540692731ebea68e26e
wongleer <770013479@qq.com>
2023-04-10 19:41:05 +0800

    完成了商品列表页面功能的开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,76 @@
+<template>
+	<view>
+		    <view class="goods-item">
+		      <!-- 左侧区域 -->
+		      <view class="goods-item-left">
+		        <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+		      </view>
+		      <!-- 右侧区域 -->
+		      <view class="goods-item-right">
+		        <!-- 标题区 -->
+		        <view class="goods-name">{{goods.goods_name}}</view>
+		        <!-- 商品价格区 -->
+		        <view class="goods-info-box">
+		          <view class="goods-price">
+		            ￥{{goods.goods_price | finPrice}}
+		          </view>
+		        </view>
+		      </view>
+		    </view>
+	</view>
+</template>
+
+<script>
+	export default {
+    filters:{
+      finPrice(price){
+        return Number(price).toFixed(2)
+      }
+    },
+    props:{
+      goods:{
+        type:Object,
+        default:{}
+      }
+    },
+		data() {
+			return {
+        // 默认图片
+        defaultPic:'https://cn.bing.com/images/search?view=detailV2&ccid=uxjZdohr&id=BBB42B43B5936E4A362AB7B05EA3EFF65EABD571&thid=OIP.uxjZdohr18bpdGrp-YhNAQHaHT&mediaurl=https%3a%2f%2fuploadfile.bizhizu.cn%2f2015%2f0518%2f20150518032433289.jpg&exph=988&expw=1002&q=%e6%87%92%e6%b4%8b%e6%b4%8b%e5%9b%be%e7%89%87&simid=608046784095138084&FORM=IRPRST&ck=4075F367935DE65A7E9D52574DD0F4EF&selectedIndex=0&idpp=overlayview&ajaxhist=0&ajaxserp=0'
+			};
+		},
+    // methods
+    
+	}
+</script>
+
+<style lang="scss">
+.goods-item{
+  display: flex;
+  padding: 10px 5px;
+  border-bottom: 1px solid #f0f0f0;
+  .goods-item-left{
+    margin-right: 5px;
+    .goods-pic {
+      width: 100px;
+      height: 100px;
+      display: block;
+    }
+  }
+  .goods-item-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .goods-name{
+      font-size: 13px;
+    }
+    .goods-info-box {
+      .goods-price {
+        font-size: 16px;
+        color: #C00000;
+      }
+    }
+  }
+}
+</style>
+
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
-		Goods_list
+    <!-- 大容器,包含所有 -->
+    <view class="goods-list">
+    <view v-for="(goods,index) in goodsList" :key="index" @click="gotoDetail(goods.goods_id)">
+      <my-goods :goods="goods"></my-goods>
+    </view>
+    </view>
 	</view>
 </template>
 
@@ -8,12 +13,108 @@
 	export default {
 		data() {
 			return {
-				
+        // 请求参数对象
+        queryObj:{
+          // 查询关键字
+          query:'',
+          // 商品分类id
+          cid:'',
+          // 页码值
+          pageNum:1,
+          // 每页展示多少条数据
+          pageSize:10
+        },
+        // 商品列表数量
+        total:0,
+        // 商品列表数据
+        goodsList:[],
+        // 节流阀
+        isLoading:false
 			};
-		}
+		},
+    // methods
+    methods:{
+      // 发请求获取商品列表数据
+     async getGoodsList(cb){
+       // 打开节流阀
+     this.isLoading=true
+     let result= await  uni.$http.get('/api/public/v1/goods/search',this.queryObj)
+     // 关闭节流阀
+     this.isLoading=false
+     cb && cb()
+     if(result.data.meta.status!==200) return uni.$showMsg()
+     this.goodsList=[...this.goodsList,...result.data.message.goods]
+     this.total=result.data.message.total
+     console.log(result);
+      },
+      // 点击item项跳往商品详情页
+      gotoDetail(id){
+        uni.navigateTo({
+          url:"/subpkg/goods_detail/goods_detail?goods_id="+ id
+        })
+      }
+    },
+    // 下拉刷新事件
+    onPullDownRefresh(){
+      this.queryObj.pageNum=1;
+      this.total=0;
+      this.goodsList=[],
+      this.isLoading=false;
+      
+      // 重写发起请求
+      this.getGoodsList(() => {
+        return uni.stopPullDownRefresh()
+      })
+    },
+    // onReachBottom
+    onReachBottom(){
+      // 判断是否数据加载完毕
+      if(this.queryObj.pageNum * this.queryObj.pageSize >= this.total) return uni.$showMsg("数据加载完毕!")
+      // 让页码自增加 1
+      this.queryObj.pageNum+=1;
+      // 重新获取数据
+      if(this.isLoading) return 
+      this.getGoodsList()
+    },
+    // onLoad
+    onLoad(options){
+      // 页面发生跳转时将query参数和cid进行赋值
+      this.queryObj.query = options.query || '';
+      this.queryObj.cid = options.cid || '';
+      console.log('queryObj',this.queryObj);
+      // 发起网络请求
+      this.getGoodsList()
+    }
+    // onLoad
 	}
 </script>
 
 <style lang="scss">
-
+.goods-item{
+  display: flex;
+  padding: 10px 5px;
+  border-bottom: 1px solid #f0f0f0;
+  .goods-item-left{
+    margin-right: 5px;
+    .goods-pic {
+      width: 100px;
+      height: 100px;
+      display: block;
+    }
+  }
+  .goods-item-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .goods-name{
+      font-size: 13px;
+    }
+    .goods-info-box {
+      .goods-price {
+        font-size: 16px;
+        color: #C00000;
+      }
+    }
+  }
+}
 </style>
